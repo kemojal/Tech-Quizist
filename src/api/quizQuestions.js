ebaf7cf21c0eb897c9c47a79be14fa5779ad5a08
Kemo Jallow <kemojal@users.noreply.github.com>
2019-01-27 15:43:45 +0800

    comit

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,123 @@
+var quizQuestions = [
+  {
+      question: "What is your favorite IDE",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "Visual Studo"
+          },
+          {
+              type: "Apple",
+              content: "Xcode"
+          },
+          {
+              type: "Linux",
+              content: "KDevelop"
+          }
+      ]
+  },
+  {
+    question: "Which operating system are you currently using?",
+    answers: [
+        {
+            type: "Microsoft",
+            content: "Windows"
+        },
+        {
+            type: "Apple",
+            content: "Mac"
+        },
+        {
+            type: "Linux",
+            content: "Linux"
+        }
+    ]
+},
+  {
+      question: "Which mobile operating system would you prefer?",
+      answers: [
+          {
+              type: "Apple",
+              content: "iPhone"
+          },
+          {
+              type: "Google",
+              content: "Android Phone"
+          },
+          {
+              type: "Microsoft",
+              content: "Windows phone"
+          }
+      ]
+  },
+  {
+      question: "Chrome or Safari ?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "Microsoft Edge"
+          },
+          {
+              type: "Google",
+              content: "Chrome"
+          },
+          {
+              type: "Apple",
+              content: "Safari"
+          }
+      ]
+  },
+  {
+      question: "What is your favorite Language?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "C#"
+          },
+          {
+              type: "Apple",
+              content: "Swift"
+          },
+          {
+              type: "Google",
+              content: "Java"
+          }
+      ]
+  },
+  {
+      question: "Which cloud storage do you use?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "One Drive"
+          },
+          {
+              type: "Google",
+              content: "Google Drive"
+          },
+          {
+              type: "Apple",
+              content: "iCloud"
+          }
+      ]
+  },
+  {
+    question: "Which one do you prefer",
+    answers: [
+        {
+            type: "Apple",
+            content: "Keynote"
+        },
+        {
+            type: "Google",
+            content: "Slides"
+        },
+        {
+            type: "Microsoft",
+            content: "Powerpoint"
+        }
+    ]
+}
+];
+
+export default quizQuestions;
